200ab0001d3b2488dc378a1a702f7f51a656f043
Vincent Kipngetich <vinniebluz@icloud.com>
2022-09-07 15:03:38 +0300

    Added User input validation and logic reset

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -9,6 +9,15 @@ const AddUser = () => {
 
   const addUserHandler = (event) => {
     event.preventDefault();
+
+    if(enteredUsername.trim().length === 0 || enteredAge.trim().length === 0){
+        return;
+    } 
+    if (+enteredAge < 1){
+        return;
+    }
+    setEnteredUsername('');
+    setEnteredAge('');
   };
 
   const usernameChangeHandler = (event) => {
@@ -23,9 +32,9 @@ const AddUser = () => {
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" onChange={usernameChangeHandler} />
+        <input id="username" type="text" onChange={usernameChangeHandler} value={enteredUsername} />
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number" onChange={ageChangeHandler} />
+        <input id="age" type="number" onChange={ageChangeHandler}  value={enteredAge}/>
         <Button type="submit">Add User</Button>
       </form>
     </Card>
